62422694daed097a0d8ae8a7cdeb78f7f35c31c5
AhmedMEltwab <117284336+AhmedMTwab@users.noreply.github.com>
2026-06-30 13:31:32 +0300

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: "https://ahmedmtwab.github.io",
+    site: "https://ahmedeltwab.com",
     base: "/Portfolio",
 
     trailingSlash: "always",
